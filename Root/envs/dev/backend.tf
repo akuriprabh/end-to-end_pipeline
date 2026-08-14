a599f9e0d79018d606e1e21160bd0d4cce29a80c
akuriprabh <36980256+akuriprabh@users.noreply.github.com>
2026-08-14 14:29:23 -0500

    Update backend.tf

--- Root/envs/dev/backend.tf
+++ Root/envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tfstatefile123test"
+    bucket         = "tfstatefile123test1"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
